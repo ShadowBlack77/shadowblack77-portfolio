db4ab0a3fd01bcf512d107428f2b29fa03442f76
ShadowBlack <daniel.miliczek@op.pl>
2024-01-28 15:38:44 +0100

    About Animation

--- portfolio/src/app/components/about/about.component.ts
+++ portfolio/src/app/components/about/about.component.ts
@@ -1,10 +1,56 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  animations: [
+    trigger('aboutAnimation', [
+      state('start', style({
+        opacity: 0
+      })),
+      state('end', style({
+        opacity: 1
+      })),
+      transition('start => end', [
+        animate('1s ease-in-out')
+      ]),
+      transition('end => start', [
+        animate('1s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AboutComponent {
+  @ViewChild('aboutElement') aboutElement!: ElementRef;
 
+  animationState = 'start';
+
+  checkIfElementIsInViewport() {
+    const element = this.aboutElement.nativeElement;
+    const rect = element.getBoundingClientRect();
+
+    const isInViewport = 
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+    if (isInViewport) {
+      this.animationState = 'end';
+    } else {
+      // this.animationState = 'start';
+    }
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.checkIfElementIsInViewport();
+  }
+
+  @HostListener('window:load')
+  onLoad() {
+    this.checkIfElementIsInViewport();
+  }
 }
--- portfolio/src/app/components/services/services.component.ts
+++ portfolio/src/app/components/services/services.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild, HostListener } from "@angular/core";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-services',
