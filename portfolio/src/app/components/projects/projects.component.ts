2b216eb0cf56eff0c0570f22eb62b1d8367baaf3
ShadowBlack <daniel.miliczek@op.pl>
2024-01-29 13:50:05 +0100

    complete theme web

--- portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/src/app/components/projects/projects.component.ts
@@ -1,10 +1,10 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-  
+
 }
